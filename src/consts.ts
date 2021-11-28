96c0a22dd1196012575593a36360d48c79ace224
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2021-11-27 20:47:54 -0500

    fix broken link

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import { LaunchOptions } from 'playwright';
 
-export const SITE_URL = 'https://scena-app--master-1c1b3b2-c72u6chy.web.app';
+export const SITE_URL = 'https://scena-app--master-639ff59-hnj3mkaw.web.app/';
 
 export const BASE_ARGS = ['--no-sandbox', '--ignore-certificate-errors'];
 
