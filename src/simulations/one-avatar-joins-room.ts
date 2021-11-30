8b0f371efd8c0f8b9a466cf300e6f7d2b6ae8e4e
Eric Weitzman <Eweitzman@edu.uwaterloo.ca>
2021-11-30 11:18:34 -0500

    added random movement algorithm using playwright keyboard

--- src/simulations/one-avatar-joins-room.ts
+++ src/simulations/one-avatar-joins-room.ts
@@ -3,6 +3,7 @@
 import playwright from 'playwright';
 import { launchPage, sleep } from '../helpers';
 import { joinRoom } from '../helpers/joinRoom';
+import { randomNumberBetween } from '../helpers/utils';
 
 declare const N360: any;
 // declare const SCENA_ENV: any;
@@ -24,14 +25,41 @@ export const oneAvatarJoinsRoom = async () => {
           localAvatarRig!.setAttribute('position', '0 0 0');
         });
 
-        // make avatar move back and forth using A-frame animations
-        await page.evaluate(() => {
-          const localAvatarRig = document.querySelector('#local-avatar-rig');
-          localAvatarRig!.setAttribute(
-            'animation',
-            'property: position; from: 5 0 0; to: -5 0 0; dir: alternate; dur: 2000; easing: easeInOutQuad; loop: true',
-          );
-        });
+        console.log('before loop');
+        var counter = 0;
+        while(counter < 300) {
+             var randTime = randomNumberBetween(1, 5);
+             counter = counter + randTime;
+             let randDirection : number = randomNumberBetween(1, 4);
+             var direction = '';
+            console.log('moving for ' + randTime + ' seconds');
+             switch(randDirection) {
+                case 1:
+                    console.log('going left');
+                    direction = 'ArrowLeft';
+                break;
+
+                case 2:
+                    console.log('going up');
+                    direction = 'ArrowUp';
+                break;
+
+                case 3:
+                    console.log('going right');
+                    direction = 'ArrowRight';
+                break;
+
+                case 4:
+                    console.log('going down');
+                    direction = 'ArrowDown';
+                break;
+             }
+            console.log('almost end loop');
+            await page.keyboard.down(direction);
+            await sleep(1000 * randTime);
+            await page.keyboard.up(direction);
+            console.log('end loop');
+        }
 
         await sleep(30000); // sleep for 30 seconds
       } finally {
